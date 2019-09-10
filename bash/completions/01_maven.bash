91643a0d9b297c7f435435be6bacf6f18e9a99e9
KnockHarder <channingwong@qq.com>
2019-09-11 02:30:03 +0800

    /amend 修复mvn在proxychain下补全异常的问题

--- bash/completions/01_maven.bash
+++ bash/completions/01_maven.bash
@@ -1,6 +1,6 @@
 #!/bin/bash 
 # https://github.com/Stubbs/maven-bash-completion
-#
+
 _mvn(){
     local cmds="archetype:create archetype:generate archetype:create-from-project \
         archetype:crawl clean validate compile test package integration-test \
@@ -16,22 +16,21 @@ _mvn(){
 
     if [ $COMP_CWORD -gt 1 ]; then
 	local pre_word=${COMP_WORDS[$(expr $COMP_CWORD - 1 )]}
-	if [ $cur == : ]; then
+	if [ "$cur" == : ]; then
 	    COMPREPLY=( $(compgen -W "$cmds" ${pre_word}:) )
 	    local i=${#COMPREPLY[*]}
 	    while [ $((--i)) -ge 0 ]; do
 		COMPREPLY[$i]=${COMPREPLY[$i]#*$pre_word:} 
 	    done
 	    return 0
-	else if [ $pre_word == : ]; then
-		 local pp_word=${COMP_WORDS[$(expr $COMP_CWORD - 2)]}
-		 COMPREPLY=( $(compgen -W "$cmds" ${pp_word}:${cur}) )
-		 local i=${#COMPREPLY[*]}
-		 while [ $((--i)) -ge 0 ]; do
-		     COMPREPLY[$i]=${COMPREPLY[$i]#*$pp_word:} 
-		 done
-		 return 0
-	     fi
+	elif [ $pre_word == : ]; then
+	    local pp_word=${COMP_WORDS[$(expr $COMP_CWORD - 2)]}
+	    COMPREPLY=( $(compgen -W "$cmds" ${pp_word}:${cur}) )
+	    local i=${#COMPREPLY[*]}
+	    while [ $((--i)) -ge 0 ]; do
+		COMPREPLY[$i]=${COMPREPLY[$i]#*$pp_word:} 
+	    done
+	    return 0
 	fi
     fi
     
