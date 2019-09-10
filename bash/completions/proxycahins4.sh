34e8f8aac21d8409fe528fd00edde133b2fee5b6
KnockHarder <channingwong@qq.com>
2019-09-11 00:23:12 +0800

    修改proxychains对应的补全脚本

--- bash/completions/proxycahins4.sh
+++ bash/completions/proxycahins4.sh
@@ -1,50 +1,34 @@
 #!/bin/bash
-_match_words(){
-    local command_all=($1)
-    local word=$2
-    
-    local end=${#word}
-    if [ $end -eq 0 ]; then
-	COMPREPLY=( ${command_all[*]} )
-	return
-    fi
-    for com in ${command_all[*]}; do
-	if [[ "$com" == "$word"* ]]; then
-	    COMPREPLY=( ${COMPREPLY[*]} $com )
-	fi
-    done
-    if [ ${COMPREPLY[0]} == "" ]; then
-	COMPREPLY=
-    fi
-}
 
-_comp_proxy() {
-    local word=${COMP_WORDS[$COMP_CWORD]}
-    local prefix=${COMP_WORDS[$COMP_CWORD-1]}
-    local path_all=$( echo $PATH | sed 's/:/ /g' )
-    local path=
-    local command_all=
-    for path in ${path_all[*]}; do
-    	command_all=( ${command_all[*]} $( ls $path ) )
-    done
+_proxychains() {
+    local cur=${COMP_WORDS[$COMP_CWORD]}
+
     case $COMP_CWORD in
     	1)
-	    _match_words "${command_all[*]}" $word
+	    local path_all=$( echo $PATH | sed 's/:/ /g' )
+	    local command_all=
+	    local path=
+	    for path in ${path_all[*]}; do
+    		command_all=${command_all[*]}\ $( ls $path )
+	    done
 	    ;;
 	2)
-	    if [ $prefix == git ]; then
-    		command_all=( fetch pull push )
+	    pre_word=${COMP_WORDS[1]}
+	    if [ $pre_word == "git" ]; then
+    		command_all="fetch pull push"
 	    else
-		if [ $prefix == ssh ]; then
-		    command_all=( wangzhiqiang@172.16.192.199 )
+		if [ $pre_word == "ssh" ]; then
+		    command_all="wangzhiqiang@172.16.192.199"
 		fi
 	    fi
-	    _match_words "${command_all[*]}" $word
 	    ;;
 	*)
-	    COMPREPLY=
+	    COMPREPLY=()
+	    return 0
 	    ;;
+	
     esac
+    COMPREPLY=( $(compgen -W "$command_all" $cur))
 }
 
-complete -F _comp_proxy proxychains4
+complete -F _proxychains proxychains4
