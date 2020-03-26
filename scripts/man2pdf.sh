3b874ca9a66d09a9e15a4a7430d5d1afe7fe3ade
KnockHarder <channingwong@qq.com>
2020-03-26 16:57:47 +0800

    支持批量

--- scripts/man2pdf.sh
+++ scripts/man2pdf.sh
@@ -7,8 +7,12 @@ fi
    
 if [ $# -lt 1 ]
 then
-   echo USAGE $0 COMMAND
+   echo USAGE $0 COMMAND...
    exit
 fi
 
-man -t $1 | ps2pdf - $PDF_DIR/$1.pdf
+for cmd in $@
+do
+    man -t ${cmd} | ps2pdf - $PDF_DIR/${cmd}.pdf
+done
+
